73dfc82fc3eb4c23ded3a31ebe390b9327ba52f6
Scott Fraser <scottfraserdev@gmail.com>
2021-07-18 23:15:16 -0700

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="nav">
+    <button>
+      <h1>Join the club</h1>
+    </button>
+  </div>
+  <img alt="Vue logo" src="./assets/logo.jpg" />
+  <Main msg="Welcome to Your Vue.js App" />
+</template>
+
+<script>
+import Main from "./components/Main.vue";
+
+export default {
+  name: "App",
+  components: {
+    Main
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#nav {
+  width: 99vw;
+  display: flex;
+  justify-content: flex-end;
+}
+
+button {
+  border: none;
+  padding: 1em 2em;
+  border-radius: 20px;
+}
+button h1 {
+  padding: none;
+}
+
+img {
+  max-width: 90vw;
+  padding: 2em;
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,56 @@
+<template>
+  <header className="App-header">
+    <h1>🛥️</h1>
+    <h4>Keeping it classy since 2021</h4>
+  </header>
+  <div id="main">
+    <div class="card">
+      <h3>T-Shirt</h3>
+    </div>
+    <div class="card">
+      <h3>Hat</h3>
+    </div>
+    <div class="card">
+      <h3>T-Shirt</h3>
+    </div>
+    <div class="card">
+      <h3>Hat</h3>
+    </div>
+  </div>
+
+  <footer>
+    <p>&copy; {{ date }} EPYC</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data: function() {
+    return {
+      date: new Date().getFullYear()
+    };
+  },
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
